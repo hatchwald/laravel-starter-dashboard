2072799da63d29ff2c125ece0585986bc6cd471a
Hatchwald <fatkhur139wec@gmail.com>
2024-02-05 10:34:33 +0700

    feat: make seeder roles

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = Role::create(['name' => 'SUPERADMIN']);
+        $permissionAdmin = [];
+        for ($i = 1; $i <= 12; $i++) {
+            $permissionAdmin[] = $i;
+        }
+        $roles->syncPermissions($permissionAdmin);
+
+        $user = Role::create(['name' => 'USER', 'guard_name' => 'web']);
+        $permissionUser = [];
+        for ($i = 9; $i <= 12; $i++) {
+            $permissionUser[] = $i;
+        }
+        $user->syncPermissions($permissionUser);
+    }
+}
